630405b7c5e18f3c75b7110b5b649f84ea762dd3
premnath-s2001 <premath.2001.s@outlook.com>
2022-04-23 11:33:16 +0530

    first commit

---
+++ src/components/Category.jsx
@@ -0,0 +1,69 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import React from "react";
+
+function Category() {
+  return (
+    <List>
+      <Slink to={"/cuisine/Italian"}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </Slink>
+      <Slink to={"/cuisine/American"}>
+        <FaHamburger />
+        <h4>American</h4>
+      </Slink>
+      <Slink to={"/cuisine/Thai"}>
+        <FaPizzaSlice />
+        <h4>Thai</h4>
+      </Slink>
+      <Slink to={"/cuisine/Japanese"}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </Slink>
+    </List>
+  );
+}
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 3rem 0rem;
+`;
+
+const Slink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+
+  width: 10rem;
+  height: 10rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-style: 0.8rem;
+  }
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+    svg {
+      color: white;
+    }
+    h4 {
+      color: white;
+    }
+  }
+`;
+export default Category;
